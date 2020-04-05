fe34e3270f1b8eb84c4c81d8bbfd74e4763f2e3e
Kunal Booch <kunalbooch@hotmail.com>
2020-04-05 18:28:00 +0530

    .idea

--- recommendation.py
+++ recommendation.py
@@ -22,9 +22,9 @@ similar_movies = list(enumerate(cosine_sim[movie_index]))
 sorted_similar_movies = sorted(
     similar_movies, key=lambda x: x[1], reverse=True)[1:]
 i = 0
-print("Top 5 similar movies to "+movie_user_likes+" are:\n")
+print("Top 7 similar movies to "+movie_user_likes+" are:\n")
 for element in sorted_similar_movies:
     print(get_title_from_index(element[0]))
     i = i+1
-    if i >= 5:
+    if i >= 7:
         break
